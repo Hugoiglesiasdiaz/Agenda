24cd352cf7dd6f062ec86ff176eccefc2f43f23c
HugoIglesiasDíaz1 <hugoiglesiasdiaz1@gmail.com>
2022-12-14 14:14:18 +0100

    new file:   .vscode/settings.json 	new file:   README.md 	new file:   bin/Agenda.class 	new file:   bin/Persona.class 	new file:   src/Agenda.java 	new file:   src/Persona.java

---
+++ src/Agenda.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Agenda {
+    static Persona[] contactos = new Persona[0];
+
+
+    public static void main(String[] args) {
+        int opcion = 0;
+        Scanner sc = new Scanner(System.in);
+        do{
+            imprimirMenu();
+            //Leer opcion
+            opcion = sc.nextInt();
+            //ejecutar opción
+            switch(opcion){
+                case 1:anhadirContacto();break;
+                case 2:listarContactos();break;
+                case 3:break;
+                case 4:break;
+                case 5:break;
+            }
+        }while(opcion != 0);
+    }
+
+    static public void imprimirMenu(){
+        System.out.println("\nMENU");
+        System.out.println("=====\n");
+        System.out.println("1. Añadir contacto");
+        System.out.println("2. Listar contactos");
+        System.out.println("3.Eliminar contactos");
+        System.out.println("4. Vaciar agenda");
+        System.out.println("5. Buscar contacto");
+        System.out.println("0. Salir");
+    }
+
+    static public void anhadirContacto(){
+        Scanner sc = new Scanner(System.in);
+        Persona p = new Persona();
+
+        System.out.print("Nombre: ");
+        p.setNombre(sc.nextLine() );
+        System.out.print("Apellidos: ");
+        p.setApellidos(sc.nextLine() );
+        System.out.print("Teléfono: ");
+        p.setTelefono(sc.nextLine() );
+        System.out.print("mail: ");
+        p.setMail(sc.nextLine() );
+
+        contactos = Arrays.copyOf(contactos, contactos.length + 1);
+        contactos[contactos.length - 1] = p;
+
+    }
+
+    static public void listarContactos(){
+        System.out.println(Arrays.toString(contactos));
+    }
+
+}
---
+++ src/Persona.java
@@ -0,0 +1,39 @@
+public class Persona {
+    private String nombre;
+    private String apellidos;
+    private String telefono;
+    private String mail;
+
+    
+
+
+
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellidos() {
+        return apellidos;
+    }
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    public String getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    public String getMail() {
+        return mail;
+    }
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    
+
+}
